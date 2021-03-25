de82f41cfae6f4df13560c89d0774dadc8d0b391
galusgaming <kaxtom29@gmailcom>
2021-03-25 23:27:40 +0100

    create clock system with auto renaming

--- src/commands/clock.command.js
+++ src/commands/clock.command.js
@@ -1,16 +1,31 @@
 module.exports = {
-    name:"clock",
-    description:"Wyświetla zegar!",
-    args: true,
-    guildOnly: true,
-    usage: "<action>[add/remove]",
+  name: "clock",
+  description: "Clock command.",
+  args: true,
+  guildOnly: true,
+  usage: "<action>[add]",
 
-    run (msg, args) {
-        const {channel, guild} =msg
-    
-        const time =new Date().toLocaleTimeString().slice(0,5)
-        const channelName =  "⌚"
+  async run(msg, args) {
+    const { channel, guild, client } = msg
 
-        guild.createChannel(channelName)
-    },
+    // const time = new Date().toLocaleTimeString().slice(0, 5)
+    const time = new Date().toLocaleTimeString()
+    const channelName = `🕥 ${time}`
+
+    const createdChannel = await guild.channels.create(channelName, {
+      type: "voice",
+    })
+
+    if (createdChannel) {
+      const channelId = createdChannel.id
+
+      const { settings } = client
+      // Save channel id to config
+      if (!settings.get(guild.id)) {
+        settings.set(guild.id, { clocks: [] })
+      }
+      client.settings.get(guild.id).clocks.push(channelId)
+      client.saveConfig(guild.id)
+    }
+  },
 }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,63 @@
-const chalk = require('chalk');
-const Discord = require('discord.js');
-const {token} = require('./config/config.js');
-const { Client, MessageEmbed,} = require('discord.js');
-const client = new Client();
+const { Client } = require("discord.js")
+const chalk = require("chalk")
+
+const { token } = require("./config/config.js")
+
+const client = new Client()
+
 const commandHandler = require("./handlers/command.handler")
+const settingsHandler = require("./handlers/settings.handler")
 
-const log  = console.log
+const log = console.log
 
-client.on('ready', () => {
-  log(chalk.green(`Zalogowałeś się jako... -> `+chalk.red.bold( ` ${client.user.tag}!`)));
-});
-
+// Initialize Comamnd Manager
 commandHandler(client)
+// Initialize Settings Manager
+settingsHandler(client)
+
+client.on("ready", () => {
+  log(chalk.green(`Zalogowałeś się jako... -> `+chalk.red.bold( ` ${client.user.tag}!`)));
+
+  // Initialize interval for each guild
+  client.settings.forEach((config, guildId) => {
+    const { guilds } = client
+    // Check if guild exist
+    if (guilds.cache.has(guildId)) {
+      const guild = guilds.cache.get(guildId)
+      // Check if available
+      if (guild.available) {
+        // console.log("available")
+
+        // Set Interval for each channel
+        const clockChannels = config.clocks
+        setInterval(() => {
+          const time = new Date().toLocaleTimeString().slice(0, 5)
+          const channelName = `🕥 ${time}`
+
+          clockChannels.forEach((channelId, index) => {
+            // Check if channel exists
+            if (guild.channels.cache.has(channelId)) {
+              // log("channel exist")
+              const channelToUpdate = guild.channels.cache.get(channelId)
+              channelToUpdate.setName(channelName)
+            } else {
+              // log("not exist")
+              // Remove Id from config
+              // that does not exist
+              clockChannels.splice(index, 1)
+              client.saveConfig(guildId)
+            }
+          })
+        }, 1000)
+      }
+    }
+  })
+})
+
+// Connect with Discord
 client.login(token)
+
+// Error handler - omit crashed
 client.on("debug", () => {})
 client.on("warn", () => {})
 client.on("error", () => {})
- 
