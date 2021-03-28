23631e04cc4d678dcd9131932ae418fdcc3c553c
galusgaming <45981986+galusgaming@users.noreply.github.com>
2021-03-28 17:46:23 +0200

    .

--- src/config/config.js
+++ src/config/config.js
@@ -4,6 +4,6 @@ module.exports = {
     token: process.env.token,
     prefix: "?",
     botAuthor: "GalusGaming",
-    botVersion: "0.2v",
+    botVersion: "0.5v",
     description: "Bot stworzony w celu lepszego poznania JS, oraz na moj server discord"
 }
