08ccfefef43f4adce3c3b8056cf5c246aad92d27
galusgaming <45981986+galusgaming@users.noreply.github.com>
2021-05-20 00:44:55 +0200

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv").config()
 
 module.exports = {
-    token: "process.env.token",
+    token: process.env.token,
     prefix: "?",
     botAuthor: "GalusGaming",
     botVersion: "0.5v",
