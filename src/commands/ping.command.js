78c97aee0fa446a7e94b8e31f85f100af619675e
galusgaming <kaxtom29@gmail.com>
2021-05-19 02:00:21 +0200

    new ping command

--- src/commands/ping.command.js
+++ src/commands/ping.command.js
@@ -1,8 +1,12 @@
+const { Client } = require("discord.js");
+
+const client = new Client
 module.exports = {
+
     name:"ping",
     description:"Ping!",
 
     run (msg) {
-        msg.reply("pong!")
+        msg.channel.send(`🏓Latency is ${Date.now() - msg.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
     }, 
 }
