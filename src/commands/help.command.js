6cfd8dcfd4481f07556e34b5dbc8626bca5b83a6
galusgaming <kaxtom29@gmail.com>
2021-10-19 19:38:47 +0200

    Revert "new help command"
    This reverts commit d062fda304cd2974df093719d79a584f28b33078.

--- src/commands/help.command.js
+++ src/commands/help.command.js
@@ -1,51 +1,73 @@
-const { MessageEmbed, Collection } = require("discord.js");
-const config = require("../config/config");
-const ee = require("../config/embed.json");
 module.exports = {
-    name: "help",
-    aliases: ["h", "commandinfo", "cmds", "cmd"],
-    cooldown: 4,
-    description: "Returns all Commmands, or one specific command",
-    run: (client, message, args, user, text, prefix) => {
+  name: "help",
+  description:
+    "List all commands the bot has or info about a specific command.",
+  usage: "[command name]",
+  guildOnly:true,
+  cooldown: 5,
 
-      const cmdList = new Collection()
+  run(msg, args) {
+    const { client: { commands },} = msg
 
+    const data = []
+    const { channel, guild, client } = msg
+    const { settings } = client
 
-      if (!args) {
-        const embed = new MessageEmbed();
-        const cmd = client.commands.get(args[0].toLowerCase()) || client.commands.get(client.aliases.get(args[0].toLowerCase()));
-        if (!cmd) {
-            return message.channel.send(embed.setColor(ee.wrongcolor).setDescription(`No Information found for command **${args[0].toLowerCase()}**`));
-        }
-        if (cmd.name) embed.addField("**Komenda :**", `\`${cmd.name}\``);
-        if (cmd.name) embed.setTitle(`Szczegółowe informacje o:\`${cmd.name}\``);
-        if (cmd.description) embed.addField("**Opis**", `\`${cmd.description}\``);
-        if (cmd.aliases) embed.addField("**aliasy**", `\`${cmd.aliases.map((a) => `${a}`).join("`, `")}\``);
-        if (cmd.cooldown) embed.addField("**Cooldown**", `\`${cmd.cooldown} Seconds\``);
-        else embed.addField("**Cooldown**", `\`${config.defaultCommandCooldown}\``);
-        if (cmd.usage) {
-            embed.addField("**Użycie**", `\`${config.prefix}${cmd.usage}\``);
-            embed.setFooter("Składnia: <> = required, [] = optional");
-        }
-         if (cmd.usage) {
-            embed.addField("**Użycie**", `\`${config.prefix}${cmd.usage}\``);
-            embed.setFooter("Składnia: <> = required, [] = optional");
-        }
-        return message.channel.send(embed.setColor(ee.color));
-      } else {
-        const embed = new MessageEmbed()
-            .setColor(ee.color)
-            .setThumbnail(client.user.displayAvatarURL())
-            .setTitle("HELP MENU 🔰 Komendy")
-            .setFooter(`Żeby zobaczyć opis i inne informacje, wpisz: ?help [CMD NAME]`, client.user.displayAvatarURL());
-        const commands = (category) => {
-            return client.commands.map((cmd) => `\`${cmd.name}\``);
-        };
-
-            embed.addField(`**${current.toUpperCase()} [${items.length}]**`, `> ${result[0].join("\n> ")}`, true);
-            embed.addField(`\u200b`, `${result[1].join("\n") ? result[1].join("\n") : "\u200b"}`, true);
-            embed.addField(`\u200b`, `${result[2].join("\n") ? result[2].join("\n") : "\u200b"}`, true);
-          }
-     
-      }
+    if (!settings.get(guild.id)) {
+      
     }
+    let Prefix = client.settings.get(guild.id).Prefix
+    const prefix = Prefix
+    // =====================================
+    //
+    // No arguments provided
+    //
+    // =====================================
+    if (!args.length) {
+      // Create list with all commands
+      data.push("To jest lista moich komend:`")
+      data.push(commands.map((command)  => command.name).join("`,` "))
+      data.push(
+        `\`\nMożesz wysłać \`${prefix}help [command name]\` po dokłaniejsze informacje o komendzie!`,
+      )
+
+      return msg.author
+        .send(data, { split: true })
+        .then(() => {
+          if (msg.channel.type === "dm") return
+          msg.reply("```Lista komend, które posiadam została wysłana na DM'a!```")
+        })
+        .catch((err) => {
+          console.error(`nie mogłem wysłać DM'a do ${msg.author.tag}.\n`, err)
+          msg.reply("Widzę, że nie mogę wysłać do ciebie wiadomośći!' \n Masz włączone wiadomości prywatne?")
+        })
+    }
+
+    // =====================================
+    //
+    // Arguments provided
+    //
+    // =====================================
+    const name = args[0].toLowerCase()
+    const command =
+      commands.get(name) ||
+      commands.find((c) => c.aliases && c.aliases.includes(name))
+
+    if (!command) {
+      return msg.reply("that's not a valid command!")
+    }
+
+    data.push(`**Name:** ${command.name}`)
+
+    if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(", ")}`)
+    if (command.description)
+      data.push(`**Description:** ${command.description}`)
+    if (command.usage)
+      data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`)
+
+    data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`)
+
+    msg.channel.send(data, { split: true })
+  },
+}
+
